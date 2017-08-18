fee5a722eb79924202c300a0d8e9081c24020da5
ChangYong <lcy9002@naver.com>
2017-08-18 09:20:47 +0900

    purchaseProduct 모델에 로직 추가

--- src/main/java/com/yong/domain/PurchaseProduct.java
+++ src/main/java/com/yong/domain/PurchaseProduct.java
@@ -19,16 +19,26 @@ public class PurchaseProduct {
     private Purchase purchase;
 
     @Column(name = "order_price")
-    private long price;
+    private long orderPrice;
 
     @Column(name = "order_count")
     private int count;
 
-    public void cancel() {
+    public static PurchaseProduct createPurchaseProduct(Product product, long orderPrice, int count){
+        PurchaseProduct purchaseProduct = new PurchaseProduct();
+        purchaseProduct.product = product;
+        purchaseProduct.orderPrice = orderPrice;
+        purchaseProduct.count = count;
 
+        product.removeStock(count);
+        return purchaseProduct;
+    }
+
+    public void cancel() {
+        this.product.addStock(this.count);
     }
 
     public long getTotalPrice(){
-        return price * count;
+        return orderPrice * count;
     }
 }
