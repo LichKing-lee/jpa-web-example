3fd63591277fbf4e6f55d0433ee6d21137f6239d
ChangYong <lcy9002@naver.com>
2017-08-18 09:07:46 +0900

    product integration test

--- src/test/java/com/yong/service/ProductServiceTest.java
+++ src/test/java/com/yong/service/ProductServiceTest.java
@@ -1,17 +1,33 @@
 package com.yong.service;
 
-import com.yong.repository.ProductRepository;
+import com.yong.config.spring.AppConfig;
+import com.yong.domain.Album;
+import com.yong.domain.Product;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 @RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = AppConfig.class)
 @Transactional
 public class ProductServiceTest {
     @Autowired
     private ProductService productService;
 
-    @Autowired
-    private ProductRepository productRepository;
+    @Test
+    public void 상품등록(){
+        Product product = new Album();
+
+        productService.saveProduct(product);
+
+        Product find = productService.findOne(product.getId());
+
+        assertThat(find, is(product));
+    }
 }
